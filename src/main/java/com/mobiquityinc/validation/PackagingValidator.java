04072c2e6c85c55e251275e6eea63ac93198815d
abhijeetshiralkar <abhijeetshiralkar@abhijeets-mbp>
2019-02-17 18:59:56 +0000

    Tested the validation

--- src/main/java/com/mobiquityinc/validation/PackagingValidator.java
+++ src/main/java/com/mobiquityinc/validation/PackagingValidator.java
@@ -18,28 +18,31 @@ public class PackagingValidator {
 	 * @throws APIException
 	 */
 	public void performValidation(Map<Double, List<PackagingItem>> packagingItemsMap) throws APIException {
+		int i = 1;
 		for (final Map.Entry<Double, List<PackagingItem>> entry : packagingItemsMap.entrySet()) {
 			final Double weightLimit = entry.getKey();
 			final List<PackagingItem> items = entry.getValue();
 			if (weightLimit > 100) {
-				throw new APIException(String.format("Weight limit %d is greater than 100", weightLimit));
+				throw new APIException(
+						String.format("Weight limit %.2f is greater than 100 on line %d", weightLimit, i));
 			}
 			if (items.size() > 15) {
-				throw new APIException(
-						String.format("Number of items to chose from is %d which is more than 15", items.size()));
+				throw new APIException(String.format(
+						"Number of items to chose from is %d which is more than 15 on line %d", items.size(), i));
 			}
 			for (final PackagingItem item : items) {
 				final Double weight = item.getWeight();
 				final Double price = item.getPrice();
 				if (weight > 100) {
-					throw new APIException(String.format("Weight of item %d is %d which is more than 100",
-							item.getIndexNumber(), item.getWeight()));
+					throw new APIException(String.format("Weight of item %d is %.2f which is more than 100 on line %d",
+							item.getIndexNumber(), item.getWeight(), i));
 				}
 				if (price > 100) {
-					throw new APIException(String.format("Price of item %d is %d which is more than 100",
-							item.getIndexNumber(), item.getPrice()));
+					throw new APIException(String.format("Price of item %d is %.2f which is more than 100 on line %d",
+							item.getIndexNumber(), item.getPrice(), i));
 				}
 			}
+			i++;
 		}
 	}
 
